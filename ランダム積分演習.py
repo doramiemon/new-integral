6f78815ca44fa9322b133382931df439a97f6dbb
doramiemon <124042193+doramiemon@users.noreply.github.com>
2025-01-04 14:43:37 +0900

    Update ランダム積分演習.py

--- ランダム積分演習.py
+++ ランダム積分演習.py
@@ -10,11 +10,11 @@ button = st.button('積分を出題しますか？')
 if button: 
     n = str(random.randint(1,x))
     image = Image.open('./data/q'+n+'.png')
-    st.image(image, caption='問題'+n,use_column_width=True)
+    st.image(image, caption='問題'+n,use_container_width=True)
 
     image2 = Image.open('./data/a'+n+'.png')
     expander = st.expander('解答'+n)
-    expander.image(image2,use_column_width=True)
+    expander.image(image2,use_container_width=True)
 
 """
 ## このページでは、ランダムで無限に積分の問題を演習することができます！
